e74a016edb1046168837bb308e8127cb32f99d1b
Andree Ismodes <andreeismodes@gmail.com>
2022-09-17 03:48:59 -0500

    Proyecto terminado <3

---
+++ script.js
@@ -0,0 +1,194 @@
+// HTML Elements
+const board = document.getElementById('board');
+const scoreBoard = document.getElementById('scoreBoard');
+const startButton = document.getElementById('start');
+const gameOverSign = document.getElementById('gameOver');
+
+// Game Settings
+const gameSpeed = 250;
+const boardSize = 10;
+const squareTypes = {
+    emptySquare: 0,
+    snakeSquare: 1,
+    foodSquare: 2
+};
+
+const directions = {
+    ArrowUp: -10,
+    ArrowDown: 10,
+    ArrowRight: 1,
+    ArrowLeft: -1,
+    
+    KeyW:-10,
+    KeyS: 10,
+    KeyD: 1,
+    KeyA: -1
+};
+
+// Game Variables
+let snake;
+let score;
+let direction;
+let boardSquares;
+let emptySquares;
+let moveInterval;
+
+const drawSnake = () => {
+    snake.forEach( square => drawSquare(square, 'snakeSquare'))
+}
+
+// Rellena cada cuadrado del tablero
+// @params
+// square: Posicion del cuadrado
+// type: tipo de cuadrado (emptySqueare, snakeSquare, foodSquare)
+const drawSquare = (square, type) => {
+    const [ row, column] = square.split('');
+    boardSquares[row][column] = squareTypes[type];
+
+    const squareElement = document.getElementById(square);
+    squareElement.setAttribute('class', `square ${type}`);
+
+    if (type == 'emptySquare') {
+        emptySquares.push(square);
+    } else {
+        if (emptySquares.indexOf(square) !== -1) {
+            emptySquares.splice(emptySquares.indexOf(square), 1);
+        }
+    }
+}
+
+const moveSnake = () => {
+    const newSquare = String(
+        Number(snake[snake.length - 1]) + directions[direction]
+    ).padStart(2, '0');
+    const [row, column] = newSquare.split('');
+    
+    if (newSquare < 0 ||
+        newSquare > boardSize * boardSize ||
+        ((direction === 'ArrowRight' || direction === 'KeyD') && column == 0) ||
+        ((direction === 'ArrowLeft' || direction === 'KeyA') && column == 9 ||
+        boardSquares[row][column] === squareTypes.snakeSquare)){
+
+        gameOver();
+    } else {
+        snake.push(newSquare);
+
+        if(boardSquares[row][column] === squareTypes.foodSquare){
+            addFood();
+        } else {
+            const emptySquare = snake.shift();
+            drawSquare(emptySquare, 'emptySquare');
+        }
+        drawSnake();
+    }
+
+}
+
+const addFood = () => {
+    score++;
+    updateScore();
+    createRandomFood();
+}
+
+const gameOver = () => {
+    gameOverSign.style.display = 'block';
+    clearInterval(moveInterval);
+    startButton.disabled = false;
+}
+
+const setDirection = newDirection => {
+    direction = newDirection;
+}
+
+
+const directionEvent = key => {
+    switch (key.code) {
+        case 'ArrowUp':
+            direction != 'ArrowDown' && setDirection(key.code)
+            break;
+
+        case 'ArrowDown':
+            direction != 'ArrowUp' && setDirection(key.code)
+
+            break;
+
+        case 'ArrowLeft':
+            direction != 'ArrowRight' && setDirection(key.code)
+
+            break;
+
+        case 'ArrowRight':
+            direction != 'ArrowLeft' && setDirection(key.code)
+
+            break;     
+
+        case 'KeyW':
+            direction != 'KeyS' && setDirection(key.code)
+            break;
+
+        case 'KeyS':
+            direction != 'KeyW' && setDirection(key.code)
+
+            break;
+
+        case 'KeyA':
+            direction != 'KeyD' && setDirection(key.code)
+
+            break;
+
+        case 'KeyD':
+            direction != 'KeyA' && setDirection(key.code)
+
+            break; 
+    }
+}
+
+const createRandomFood = () => {
+    const randomEmptySquare = emptySquares[Math.floor(Math.random() * emptySquares.length)];
+    drawSquare(randomEmptySquare, 'foodSquare');
+}
+
+const updateScore = () => {
+    scoreBoard.innerText = score;
+}
+
+const createBoard = () => {
+    boardSquares.forEach( (row, rowIndex) => {
+        row.forEach( (column, columnIndex) => {
+            const squareValue = `${rowIndex}${columnIndex}`;
+            const squareElement = document.createElement('div');
+
+            squareElement.setAttribute('class', 'square emptySquare');
+            squareElement.setAttribute('id', squareValue);
+            board.appendChild(squareElement);
+            emptySquares.push(squareValue);
+
+        });
+    });
+}
+
+const setGame = () => {
+    snake = ['00', '01', '02', '03'];
+    score = 0;
+    direction = 'ArrowRight';
+
+    boardSquares = Array.from(Array(boardSize), () => new Array(boardSize).fill(squareTypes.emptySquare));
+    board.innerHTML = '';
+    emptySquares = [];
+    createBoard();
+
+}
+
+const startGame = () => {
+    setGame();
+    gameOverSign.style.display = 'none';
+    startButton.disabled = true;
+    board.style.height = '100%';
+    board.style.width = '100%';
+    drawSnake();
+    updateScore();
+    createRandomFood();
+    document.addEventListener('keydown', directionEvent);
+    moveInterval = setInterval(() => moveSnake(), gameSpeed);
+}
+startButton.addEventListener('click', startGame);
